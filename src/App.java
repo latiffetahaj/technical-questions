8fe097871aa5c4452b48e8644eaa3fe0a9bbc3c4
latiffetahaj <latiffetahaj@gmail.com>
2022-10-05 20:02:54 -0400

    reverse sentence question

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import javax.sound.midi.SysexMessage;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
         System.out.println("Hello, World!");
+
+        String s = "i live in a house";
+        String sArray[] = s.split(" ");
+
+        for(int i = 0; i<sArray.length; i++) {
+            System.out.println(sArray[i]);
+        }
     }
 }
