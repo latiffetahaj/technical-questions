9cf532da456650650b9b75815baf26fe85439062
latiffetahaj <latiffetahaj@gmail.com>
2022-11-18 22:11:55 -0500

    added description for unsorted sub array problem

--- UnSortedSubArray.java
+++ UnSortedSubArray.java
@@ -1,6 +1,20 @@
 
+/**
+ * Level: Medium
+ * Given an array of integers, find the shortest sub array, that if sorted, results in the entire array being sorted.
+ * For example:[1,2,4,5,3,5,6,7] --> [4,5,3] - If you sort from indices 2 to 4, the entire array is sorted.
+ * [1,3,5,2,6,4,7,8,9] --> [3,5,2,6,4] -  If you sort from indices 1 to 5, the entire array is sorted.
+ *
+ *
+ * /*
+ * @author Latif Fetahaj
+ * @version 1.0
+ *
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)
+ */
 
-public class Main {
+public class UnSortedSubArray {
 
     public static void unsortedSubArray(int[] a) {
 
